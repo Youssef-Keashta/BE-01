1d6ed7f8971d66f96a75ac361397caa2b69fab8c
Youssef-Keashta <Youssefkeashta@gmail.com>
2026-07-21 12:22:30 +0300

    Stage 2: Read Endpoints with 404

---
+++ Controllers/tasksController.cs
@@ -0,0 +1,38 @@
+﻿using BE_01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_01.Controllers
+{
+    [Route("tasks")]
+    [ApiController]
+    public class tasksController : ControllerBase
+    {
+        //static list initialized with 3 tasks
+        private static List<ToDoTask> Tasks = new()
+        {
+            new ToDoTask("Buy Milk"),
+            new ToDoTask ("Watch Movie"),
+            new ToDoTask ("Take Out the Trash")
+        };
+
+        //Return full list of tasks
+        [HttpGet]
+        public ActionResult GetAllTasks()
+        {
+            return Ok(Tasks);
+        }
+
+        //Return single task (by ID)
+        [HttpGet("{id}")]
+        public ActionResult GetTaskById(int id)
+        {
+            var task = Tasks.FirstOrDefault(x => x.Id == id);
+            if (task == null)
+            {
+                return NotFound(new { error = $"Task {id} not found :(" });
+            }
+            return Ok(task);
+        }
+    }
+}
---
+++ Models/ToDoTask.cs
@@ -0,0 +1,16 @@
+﻿namespace BE_01.Models
+{
+    public class ToDoTask
+    {
+        private static int _nextId = 1;
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; } = false;
+        public ToDoTask(string title)
+        {
+            this.Id = _nextId++;
+            this.Title = title;
+            this.Done = false;
+        }
+    }
+}
